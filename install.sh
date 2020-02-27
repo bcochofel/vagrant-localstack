fe1dc36538a1f054120a1ad5d310524ed7d65a58
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-27 16:24:09 +0000

    Localstack run locally

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install python3 and pip3
-sudo apt install python3 python3-pip curl wget jq
+sudo apt install python3 python3-pip python3-dev curl wget jq
 
 # install some pip3 modules
 pip3 install docker-compose
